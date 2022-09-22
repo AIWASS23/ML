3ca4308a6036e979f02c66c2ca294beb533d5cc6
AIWASS23 <marcelodearaujo677@gmail.com>
2022-09-22 15:12:47 -0300

    segunda aula

--- PDI/Aula_2/code2.py
+++ PDI/Aula_2/code2.py
@@ -1,19 +1,20 @@
 # Import the color module
-from ____ import ____
+from skimage import color
 
 # Import the filters module and sobel function
-from skimage.____ import ____
+from skimage.filters import sobel
 
 from PDI.src.pdi_utils import load_soaps_image,show_image
 
 soaps_image = load_soaps_image()
 
 # Make the image grayscale
-soaps_image_gray = ____.____(soaps_image)
+soaps_image_gray = color.rgb2gray(soaps_image)
 
 # Apply edge detection filter
-edge_sobel = ____(____)
+edge_sobel = sobel(soaps_image_gray)
 
 # Show original and resulting image to compare
 show_image(soaps_image, "Original")
 show_image(edge_sobel, "Edges with Sobel")
+
--- PDI/Aula_2/code6.py
+++ PDI/Aula_2/code6.py
@@ -2,16 +2,17 @@
 from PDI.src.pdi_utils import show_image,load_rotate_cat
 
 image_cat = load_rotate_cat()
-from skimage.___ import ___, ____
+from skimage.transform import rescale, rotate
 
 # Rotate the image 90 degrees clockwise
-rotated_cat_image = ____(image_cat, ____)
+rotated_cat_image = rotate(image_cat, angle = -90.0)
 
 # Rescale with anti aliasing
-rescaled_with_aa = ____(rotated_cat_image, 1/4, ___=__, __=__)
+rescaled_with_aa = rescale(rotated_cat_image, 1/4, preserve_range = True, anti_aliasing = True)
 
 # Rescale without anti aliasing
-rescaled_without_aa = ____
+rescaled_without_aa = rescale(rotated_cat_image, 1/4, preserve_range = True, anti_aliasing = False)
+
 
 # Show the resulting images
 show_image(rescaled_with_aa, "Transformed with anti aliasing")
