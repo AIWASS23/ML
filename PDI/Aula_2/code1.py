e1677ad91b37957bbb6349a5aebc01ef1f6a328c
AIWASS23 <marcelodearaujo677@gmail.com>
2022-10-29 11:03:45 -0300

    Estudo da aula 2 recuperado pela upload do macOS

--- PDI/Aula_2/code1.py
+++ PDI/Aula_2/code1.py
@@ -1,17 +1,18 @@
 # Import threshold and gray convertor functions
+# Import threshold and gray convertor functions
 from skimage.filters import threshold_otsu
 from skimage.color import rgb2gray
-from PDI.src.pdi_utils import show_image, load_lools_image
-gray_tools_image = rgb2gray(tools_image)
+from src.pdi_utils import show_image, load_lools_image
+tools_image = load_lools_image()
 
 # Turn the image grayscale
-gray_tools_image = color.rgb2gray()
+gray_tools_image = rgb2gray(tools_image)
 
 # Obtain the optimal thresh
 thresh = threshold_otsu(gray_tools_image)
 
 # Obtain the binary image by applying thresholding
-binary_image = gray_tools_image >thresh
+binary_image = gray_tools_image > thresh
 
 
 # Show the original image
