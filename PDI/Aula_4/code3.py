c831867fa216e4c057236cb04cbec4ef6d00b8e2
AIWASS23 <marcelodearaujo677@gmail.com>
2022-11-12 10:11:32 -0300

    Aula 4, Exercicio 3

--- PDI/Aula_4/code3.py
+++ PDI/Aula_4/code3.py
@@ -7,34 +7,34 @@ from PDI.src.pdi_utils import load_sign_language_data
 data_train , label_train , data_val , label_val, _ , _  = load_sign_language_data()
 
 #inicialização do modelo sequencial do keras
-__=___
+model =keras.Sequential()
 
 #adição de camada convolucional 2D
-__.__
+model.add(Conv2D(4, kernel_size=(3, 3), activation= 'relu',input_shape=data_train.shape[1:],padding='same'))
 
 #adição da camada de redução maxpooling
-__.__
+model.add(MaxPooling2D(pool_size=(2,2),padding='same'))
 
 #transoformação dos dados em 1 dimensão
-__.__
+model.add(Flatten())
 
 #adição da camada densa
-__.__
+model.add(Dense(29, activation = 'softmax'))
 
 #compilação do modelo
-__.__
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',metrics=['sparse_categorical_accuracy'])
 
 #definição do nome do modelo
-model_file = '__.h5'
+model_file = 'Aula.h5'
 
 #callback para salvar o melhor modelo do treinamento
-mcp_save = ModelCheckpoint(__ ,save_best_only=__, monitor='___', mode='___')
+mcp_save = ModelCheckpoint(model_file ,save_best_only=True, monitor='val_loss', mode='min')
 
 #callback para interromper o treinamento quando não existir evolução do modelo
-earlyStopping = EarlyStopping(monitor='___', patience=__, verbose=__, mode='__')
+earlyStopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='min')
 
 #callback para diminuir a taxa de aprendizagem quando os dados de acerto não evoluírem
-reduce_lr_loss = ReduceLROnPlateau(monitor='__', factor=__, patience=__, verbose=__,min_delta=1e-4,mode='__')
+reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.05, patience=5, verbose=1,min_delta=1e-4,mode='min')
 
 #treinamento do modelo
-__.__(__, __ , __, ___, ___,callbacks=[___, ___, ___])
+model.fit(data_train, label_train, validation_data = (data_val, label_val),epochs = 50, callbacks=[mcp_save, earlyStopping, reduce_lr_loss])
