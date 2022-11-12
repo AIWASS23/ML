3291040bb51dae719e5defd739441d12661eb28f
AIWASS23 <marcelodearaujo677@gmail.com>
2022-11-12 09:39:47 -0300

    Aula 4, Exercicio 2

--- PDI/Aula_4/code2.py
+++ PDI/Aula_4/code2.py
@@ -7,22 +7,22 @@ from PDI.src.pdi_utils import load_sign_language_data
 data_train , label_train , data_val , label_val, _ , _  = load_sign_language_data()
 
 #inicialização do modelo sequencial do keras
-model = keras.
+model = keras.Sequential()
 
 #adição de uma cada convolucional 2D
-model.add(___(___, kernel_size=(__, ___), activation= '___',input_shape=data_train.shape[1:],padding='___'))
+model.add(Conv2D(4, kernel_size=(3, 3), activation= 'relu',input_shape=data_train.shape[1:],padding='same'))
 
 #adição de uma camada de redução maxpooling
-model.__(___(pool_size=(__,__),____='___'))
+model.add(MaxPooling2D(pool_size=(2,2),padding='same'))
 
 #transformação dos dados em 1 dimensão
-___.___(____)
+model.add(Flatten())
 
 #adição da camada densa (MLP)
-__.__(___(__,___='___'))
+model.add(Dense(29, activation = 'softmax'))
 
 #compilação do modelo
-model.___(loss='___', optimizer='____',metrics=['sparse_categorical_accuracy'])
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',metrics=['sparse_categorical_accuracy'])
 
 # treinamento do modelo
-model.__(___, __, validation_data= (__, __), epochs=__)
+model.fit(data_train, label_val, validation_data= (data_val, label_val), epochs=5)
