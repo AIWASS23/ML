1f073064868a67de08a2c518a9f6a5a11214d8e8
AIWASS23 <marcelodearaujo677@gmail.com>
2022-11-12 11:21:43 -0300

    Aula 4, Exercicio 4

--- PDI/Aula_4/code4.py
+++ PDI/Aula_4/code4.py
@@ -28,7 +28,7 @@ model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',metrics=[
 
 
 #definição do nome do modelo
-model_name = 'PDI/Aula_4'
+model_name = 'PDI/Aula_4/aula4_code4_models'
 
 #definição do arquivo salvo do modelo
 model_file = model_name + '.h5'
--- PDI/Aula_4/code5.py
+++ PDI/Aula_4/code5.py
@@ -3,6 +3,9 @@ from tensorflow import keras
 from tensorflow.keras.callbacks import EarlyStopping,ModelCheckpoint,ReduceLROnPlateau,TensorBoard
 from tensorflow.keras.layers import Conv2D, MaxPooling2D,Flatten,Dense
 from PDI.src.pdi_utils import load_sign_language_data
+import sys
+sys.path.append(' . ')
+import os
 
 #função para salvar sumario
 def save_sumary(name):
@@ -24,48 +27,51 @@ def callbacks(model_name):
 data_train , label_train , data_val , label_val, _ , _  = load_sign_language_data()
 
 #definição da quantidade de filtros em cada camada convolucional
-conv_node=[__,__,__,__,]
+conv_node=[8,16,32,64]
 
 #definição da quantidade de neuronios da ultima camada
-output = __
+output = 29
 
 #definição da quantidade de filtros da primeira camada
-first_node = __
+first_node = 8
 
 #iteração para determinar as variações de cnn
 for cnn in  range(len(conv_node)):
 
     #inicialização do modelo sequencial do keras
-    __=__
+    model = keras.Sequential()
 
     #adição da primeira camada convolucional
-    _____
+    model.add(Conv2D(first_node, kernel_size= (3,3), activation = 'relu', padding = 'same'), input_shape = data_train.shape[1:])
 
     #adição da camada de redução maxpooling
-    ___
+    model.add(MaxPooling2D(pool_size = (2,2), padding = 'same'))
 
     #adição da camada dropout para eliminar 25% de dados aleatórios do treinamento
-    model.add(___)
+    model.add(Dropout(.25))
 
     #iteração para criar blocos adicionais de caamdas convolucionais.
     for node in conv_node[:cnn]:
         #adição de camada convolucional
-        ____
+        model.add(Conv2D(node, kernel_size = (3,3), activation = 'relu', padding = 'same'))
 
         #adição de camada de redução maxpooling
-        ____
+        model.add(MaxPooling2D(pool_size = (2,2), padding = 'same'))
 
     #transormaçao dos dados em 1 dimensão
-    ____
+    model.add(Flatten())
 
     #adição da camada de densa de saída
-    ____
+    model.add(Dense(29, activation = 'softmax'))
 
     #compilação do modelo
-    ___
+    model.compile(
+        loss = 'sparse_categorial_crossentropy',
+        optimizer = 'adam',
+        metrics = ['sparse_categorial_accuracy'])
 
     #definição do nome do modelo salvo em arquivo
-    model_file = 'models/{}-block-{}-convNode.h5'.format(cnn,conv_node[:cnn])
+    model_file = 'PDI/Aula_4/aula4_code5_models/{}-block-{}-convNode.h5'.format(cnn,conv_node[:cnn])
 
     #nome do log que será analisado pelo tensorboard
     model_name = "{}-block-{}-convNOde".format(cnn,conv_node[:cnn])
